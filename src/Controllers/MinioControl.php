365af03167065d4983bb411db01dcb65d9a03b79
developerMan93 <developer.man93@gmail.com>
2024-11-23 00:04:41 +0330

    add multiUpload

--- src/Controllers/MinioControl.php
+++ src/Controllers/MinioControl.php
@@ -21,6 +21,19 @@ class MinioControl
             return $e->getMessage();
         }
     }
+
+    public function multiUpload($files, $path): array
+    {
+        $data = [];
+        foreach ($files as $file) {
+            $content = file_get_contents($file->getRealPath());
+            $fileName = sha1(md5(time() . $file->getClientOriginalName()));
+            $path = sprintf('%s/%s.jpg', $path, $fileName);
+            $data[] = Storage::disk('minio')->put($path, $content, 'public');
+        }
+        return $data;
+    }
+
     public static function getTemporaryLink($path): string
     {
         $disk = Storage::disk('minio');
