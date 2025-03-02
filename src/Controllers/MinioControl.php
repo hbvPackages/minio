aade8ea384d745ed99808c006a721fe71a8eee49
hbv73 <vahid.habibzadeh73@gmail.com>
2025-03-02 20:13:59 +0330

    remove

--- src/Controllers/MinioControl.php
+++ src/Controllers/MinioControl.php
@@ -9,20 +9,30 @@ use Illuminate\Support\Facades\Storage;
 
 class MinioControl
 {
-    public static function upload($file, $path='public'): string
+    /**
+     * @param $file
+     * @param $path
+     * @return string
+     */
+    public static function upload($file, $path = 'public'): string
     {
         try {
             $content = file_get_contents($file->getRealPath());
             $fileName = sha1(md5(time() . $file->getClientOriginalName()));
             $path = sprintf('%s/%s.jpg', $path, $fileName);
             Storage::disk('minio')->put($path, $content, 'public');
-            return sprintf('%s/%s/%s', env('APP_URL'), 'minio/public',  $path);
+            return sprintf('%s/%s/%s', env('APP_URL'), 'minio/public', $path);
         } catch (\Throwable $e) {
             return $e->getMessage();
         }
     }
 
-    public static function multiUpload($files, $publicPath='public'): array
+    /**
+     * @param $files
+     * @param $publicPath
+     * @return array
+     */
+    public static function multiUpload($files, $publicPath = 'public'): array
     {
         $data = [];
         foreach ($files as $file) {
@@ -30,18 +40,31 @@ class MinioControl
             $fileName = sha1(md5(time() . $file->getClientOriginalName()));
             $path = sprintf('%s/%s.jpg', $publicPath, $fileName);
             Storage::disk('minio')->put($path, $content, 'public');
-            $data[] = sprintf('%s/%s/%s', env('APP_URL'), 'minio/public',  $path);
+            $data[] = sprintf('%s/%s/%s', env('APP_URL'), 'minio/public', $path);
             $content = '';
             $fileName = '';
         }
         return $data;
     }
 
+    /**
+     * @param $path
+     * @return string
+     */
     public static function getTemporaryLink($path): string
     {
         $disk = Storage::disk('minio');
         $expiration = Carbon::now()->addMinutes(30);
         return $disk->temporaryUrl($path, $expiration);
+    }
+
+    /**
+     * @param $path
+     * @return bool
+     */
+    public static function remove($path): bool
+    {
+        return Storage::disk('minio')->delete($path);
     }
 
 }
